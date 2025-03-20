34283b380abafa413d85ba06c2a89e846c3a8762
ManishPrajapat001 <prajapatmanish1002@gmail.com>
2025-03-20 23:02:13 +0530

    password secured

--- ecommerce-server-side/ecommerce/db_config.py
+++ ecommerce-server-side/ecommerce/db_config.py
@@ -1,11 +1,14 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 
 # PostgreSQL Connection URI
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/ecommerce_db'# in local db for linux no password is needed
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Manish%4042@localhost/ecommerce_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 
 db = SQLAlchemy(app)
